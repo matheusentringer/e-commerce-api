a43cb3c5bb0f3ac87f3109e8f699bb17313b18a1
Matheus Entringer <matheusentringer1@gmail.com>
2022-11-05 18:16:22 -0300

    JWT Token verification, user routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 const User = require("../models/User")
 const CryptoJS = require("crypto-js")
+const jwt = require("jsonwebtoken")
 
 //REGISTER 
 
@@ -37,9 +38,16 @@ router.post("/login", async (req, res) => {
       return res.status(401).json("Wrong credentials!")
     }
 
+    const accessToken = jwt.sign(
+      {
+        id:user._id,
+        isAdmin: user.isAdmin
+      }, process.env.JWT_SEC, {expiresIn: "3d"}
+    )
+
     const {password, ...others} = user._doc
 
-    return res.status(200).json(others)
+    return res.status(200).json({...others, accessToken})
 
   } catch(err) {
     return res.status(500).json(err)
